d00caba527d5489bfe372e42766fea2375798222
Boitumelo Rampete <u17120323@tuks.co.za>
2020-06-14 13:58:45 +0200

    LastCommit

--- src/app/modals/activity-rate/add-activity-rate/add-activity-rate.component.ts
+++ src/app/modals/activity-rate/add-activity-rate/add-activity-rate.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-add-activity-rate',
@@ -6,10 +8,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-activity-rate.component.scss']
 })
 export class AddActivityRateComponent implements OnInit {
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  rateTypes = [
+    {
+      ID: 0,
+      Name: 'Adult/Child'
+    },
+    {
+      ID: 1,
+      Name: 'Per Hut'
+    },
+    {
+      ID: 2,
+      Name: 'Bike/No Bike'
+    },
+    {
+      ID: 3,
+      Name: 'Per Person'
+    },
+    {
+      ID: 4,
+      Name: 'Per Vehicle'
+    }
+  ];
 
-  constructor() { }
+  selected: number;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.firstFormGroup = this.formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this.formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
+  }
+
+  print() {
+    console.log(this.selected);
+
   }
 
 }
