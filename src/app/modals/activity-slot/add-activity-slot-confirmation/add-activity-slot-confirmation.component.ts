c71128f52713b5661538ed57e8a57c54f2bf949c
YasheenB <u17128014@tuks.co.za>
2020-07-02 15:35:47 +0200

    Activity Slot Time Initial Commit

---
+++ src/app/modals/activity-slot/add-activity-slot-confirmation/add-activity-slot-confirmation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-activity-slot-confirmation',
+  templateUrl: './add-activity-slot-confirmation.component.html',
+  styleUrls: ['./add-activity-slot-confirmation.component.scss']
+})
+export class AddActivitySlotConfirmationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/modals/activity-slot/view-activity-slot/view-activity-slot.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { UpdateActivitySlotComponent} from 'src/app/modals/activity-slot/update-activity-slot/update-activity-slot.component';
+import { DeleteActivitySlotComponent} from 'src/app/modals/activity-slot/delete-activity-slot/delete-activity-slot.component';
+import {MatDialog} from '@angular/material/dialog'
+import { DeleteActivityComponent } from '../../activity/delete-activity/delete-activity.component';
+@Component({
+  selector: 'app-view-activity-slot',
+  templateUrl: './view-activity-slot.component.html',
+  styleUrls: ['./view-activity-slot.component.scss']
+})
+export class ViewActivitySlotComponent implements OnInit {
+
+  constructor(private dialog: MatDialog) { }
+
+  ngOnInit(): void {
+  }
+
+  updateActivitySlot(){
+    const updateActivitySlotDialog = this.dialog.open(UpdateActivitySlotComponent,{disableClose: true})
+  }
+
+  deleteActivitySlot(){
+    const deleteActivitySlotDialog = this.dialog.open(DeleteActivitySlotComponent)
+  }
+}
