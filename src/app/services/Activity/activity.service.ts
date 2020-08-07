2789990336108a69e0869e68efe120f64ffc1a46
unknown <u15347975@tuks.co.za>
2020-08-07 22:21:24 +0200

    Activity

---
+++ src/app/services/Activity/activity.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {GlobalService} from '../Global/global.service';
+
+export interface Activity{
+  ActivityID: Number,
+  ActivityTypeID: Number,
+  ActivityDescription: string,
+  ActivityMaxCapacity: Number,
+  AgeMin:Number,
+  AgeMax:Number,
+}
+
+export interface ActivityDropDown{
+  ActivityID: Number,
+  ActivityDescription: string
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ActivityService {
+
+  constructor(private global: GlobalService ,private http:HttpClient) { }
+
+  createActivity(Activity, link){
+
+  }
+  readActivity (link){
+
+  }
+  updateActivity(updatedActivity, link){  
+
+  }
+  deleteActivity (ActivityID, link){
+
+  }
+
+}
