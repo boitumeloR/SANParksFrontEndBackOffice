2a3eadbb7f87e7603390d4455c1ef21b676474fd
unknown <u15347975@tuks.co.za>
2020-08-07 21:30:22 +0200

    AccommBaseRate

---
+++ src/app/services/AccommBaseRate/accomm-base-rate.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {GlobalService} from '../Global/global.service'
+
+export interface AccommodationTypeBaseRate{
+  BaseRateID: Number,
+  AccomodationTypeID: Number,
+  CampID: Number,
+  BaseRateAmount : Number,
+ StartDate: Date,
+ EndDate: Date
+ 
+ }
+ 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccommBaseRateService {
+
+  constructor(private global: GlobalService ,private http:HttpClient ) { }
+
+  createAccommodationTypeBaseRate (AccommodationTypeBaseRate, link){
+
+  }
+  readAccommodationTypeBaseRate (link){
+
+  }
+  updateAccommodationTypeBaseRate (updatedAccommodationTypeBaseRate, link){  
+
+  }
+  deleteAccommodationTypeBaseRate (BaseRateID, link){
+    
+
+}
