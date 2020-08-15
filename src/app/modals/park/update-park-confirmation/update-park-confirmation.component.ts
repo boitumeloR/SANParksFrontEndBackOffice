774841cab933430af88d0a633d59df691c8667ed
YasheenB <u17128014@tuks.co.za>
2020-08-15 20:24:36 +0200

    Park components integrated with API endpoints

--- src/app/modals/park/update-park-confirmation/update-park-confirmation.component.ts
+++ src/app/modals/park/update-park-confirmation/update-park-confirmation.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
-import {UpdateParkSuccessfulComponent} from 'src/app/modals/park/update-park-successful/update-park-successful.component';
-import {UpdateParkUnsuccessfulComponent} from 'src/app/modals/park/update-park-unsuccessful/update-park-unsuccessful.component';
-
 @Component({
   selector: 'app-update-park-confirmation',
   templateUrl: './update-park-confirmation.component.html',
@@ -13,13 +10,5 @@ export class UpdateParkConfirmationComponent implements OnInit {
   constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
-  }
-
-  successfulUpdatePark(){
-    const updateParkSuccessfulDialog = this.dialog.open(UpdateParkSuccessfulComponent);
-  }
-
-  unsuccessfulUpdatePark(){
-    const updateParkUnsuccessfulDialog = this.dialog.open(UpdateParkUnsuccessfulComponent);
   }
 }
--- src/app/services/Park/park.service.ts
+++ src/app/services/Park/park.service.ts
@@ -1,43 +1,80 @@
 import { Injectable } from '@angular/core';
 import {GlobalService} from '../global.service';
 import { HttpClient } from '@angular/common/http';
-
-
+import { Observable , Subject} from 'rxjs';
+import { tap} from 'rxjs/operators';
+import { MatDialog } from '@angular/material/dialog';
+import {AddParkSuccessfulComponent} from 'src/app/modals/park/add-park-successful/add-park-successful.component';
+import {AddParkUnsuccessfulComponent} from 'src/app/modals/park/add-park-unsuccessful/add-park-unsuccessful.component';
+import {UpdateParkSuccessfulComponent} from 'src/app/modals/park/update-park-successful/update-park-successful.component';
+import {UpdateParkUnsuccessfulComponent} from 'src/app/modals/park/update-park-unsuccessful/update-park-unsuccessful.component';
+import { DeleteParkSuccessfulComponent } from 'src/app/modals/park/delete-park-successful/delete-park-successful.component';
+import { DeleteParkUnsuccessfulComponent } from 'src/app/modals/park/delete-park-unsuccessful/delete-park-unsuccessful.component';
 export interface Park {
-  ParkID: number,
-  ParkName: string,
-  ParkLocation: string,
-  ParkDescription: string
+  ParkID: number;
+  ParkName: string;
+  ParkLimit: number;
+  ParkLatitude: string;
+  ParkLongitude: string;
+  ParkDescription: string;
 }
 
 export interface ParkDropdown {
-  ParkID: number,
-  ParkName: string
+  ParkID: number;
+  ParkName: string;
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class ParkService {
-  constructor(private global: GlobalService, private http: HttpClient) { }
+  constructor(private global: GlobalService, private http: HttpClient, private dialog: MatDialog) { }
 
-  GetWhatever(park, service) {
-    return this.http.get(`${service}/api/Booking`);
+  private refresh = new Subject<void>();
+  get requestReferesh(){
+    return this.refresh;
   }
 
   CreatePark(Park, link){
-
+    return this.http.post('https://localhost:44371/api/Park/CreatePark', Park).pipe( tap(
+      () => {this.refresh.next(); }
+    )).subscribe((addResult: any) => {
+      if (addResult.Error){
+       const addParkUnsuccessfulDialog = this.dialog.open(AddParkUnsuccessfulComponent);
+      }
+      else{
+       const addParkSuccessfulDialog = this.dialog.open(AddParkSuccessfulComponent);
+      }
+    });
   }
 
   ReadPark(link){
-
+     return this.http.get('https://localhost:44371/api/Park/getPark');
   }
 
   UpdatePark(Park, link){
-
+    return this.http.post('https://localhost:44371/api/Park/UpdatePark', Park).pipe( tap(
+      () => {this.refresh.next(); }
+    )).subscribe((updateResult: any) => {
+      if (updateResult.Error){
+       const updateParkUnsuccessfulDialog = this.dialog.open(UpdateParkUnsuccessfulComponent);
+      }
+      else{
+       const updateParkSuccessfulDialog = this.dialog.open(UpdateParkSuccessfulComponent);
+      }
+    });
   }
 
-  DeletePark(ParkID){
-
+  DeletePark(ParkID, link){
+    return this.http.delete(`https://localhost:44371/api/Park/DeletePark?parkID=${ParkID}`).pipe( tap(
+      () => {this.refresh.next(); }
+    )).subscribe((deleteResult: any) => {
+      if (deleteResult.Error){
+        const deleteParkUnsuccessfulDialog = this.dialog.open(DeleteParkUnsuccessfulComponent);
+      }
+      else{
+        const deleteParkSuccessfulDialog = this.dialog.open(DeleteParkSuccessfulComponent);
+      }
+    });
   }
 }
