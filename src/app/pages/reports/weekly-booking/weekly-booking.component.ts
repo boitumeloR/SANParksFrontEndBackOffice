5806e836a35d1ec2548600e1bfafd10055694a35
Boitumelo Rampete <u17120323@tuks.co.za>
2020-10-06 01:14:51 +0200

    ReportArb

--- src/app/pages/reports/weekly-booking/weekly-booking.component.ts
+++ src/app/pages/reports/weekly-booking/weekly-booking.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ConfirmModalComponent } from 'src/app/modals/auxilliary-modals/confirm-modal/confirm-modal.component';
+import { AvailabilityService } from 'src/app/services/Available/availability.service';
+import { GlobalService } from 'src/app/services/Global/global.service';
+import { ReportingService } from 'src/app/services/Reports/reporting.service';
 
 @Component({
   selector: 'app-weekly-booking',
@@ -11,11 +14,20 @@ import { ConfirmModalComponent } from 'src/app/modals/auxilliary-modals/confirm-
 export class WeeklyBookingComponent implements OnInit {
 
 
-  constructor(private dialog: MatDialog, private snack: MatSnackBar) { }
+  parks: any;
+  camps: any;
+  CampID: any;
+  constructor(private dialog: MatDialog, private snack: MatSnackBar,
+              private global: GlobalService, private reportServ: ReportingService,
+              private avail: AvailabilityService) { }
 
   ngOnInit(): void {
+    this.avail.getDropDowns(this.global.GetServer()).subscribe(result => this.parks = result.Parks);
   }
 
+  GetCamps(park: any) {
+    this.avail.getCamps(park.ParkID, this.global.GetServer()).subscribe(res => this.camps = res);
+  }
   Submit() {
     const ref = this.dialog.open(ConfirmModalComponent, {
       data: {confirmMessage: 'Invalid dates, choose different dates'}
