d7e9833d4149c80c804a42c854f5f894b441ef91
Boitumelo Rampete <u17120323@tuks.co.za>
2020-10-14 04:30:54 +0200

    Deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  stripeKey: 'pk_test_51GxHggJZxYw2vGW5Fg64LK9WFpftGxGBMzQtdTewK7LgOXvcgcSE0Z7fiMv00NJRbBzBxKHqXVkWuSnyg5qHyz7p00o64kqc3o'
 };
