223227561520d3305105b09a1b729acc5d9dd781
YasheenB <u17128014@tuks.co.za>
2020-10-15 23:23:29 +0200

    nav bar removed on page not found

--- src/app/subcomponents/page-not-found/page-not-found.component.ts
+++ src/app/subcomponents/page-not-found/page-not-found.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { LoginService } from 'src/app/services/Login/login.service';
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
@@ -7,10 +8,11 @@ import { Title } from '@angular/platform-browser';
 })
 export class PageNotFoundComponent implements OnInit {
 
-  constructor(private title: Title) { }
+  constructor(private title: Title, private loginService: LoginService) { }
 
   ngOnInit(): void {
     this.title.setTitle('Page Not Found');
+    this.loginService.loggedIn.next(false);
     localStorage.clear();
   }
 }
